41ae04ca8595e138cfba1aefc08482ed764ea577
Edith123321 <edithgithinji2020@gmail.com>
2025-05-19 03:00:14 -0700

    8 lines of code

--- math/linear_algebra/0-slice_me_up.py
+++ math/linear_algebra/0-slice_me_up.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 rr = [9, 8, 2, 3, 9, 4, 1, 0, 3]
 arr1 =  # your code here
 arr2 =  # your code here
@@ -7,7 +6,4 @@ arr3 =  # your code here
 print("The first two numbers of the array are: {}".format(arr1))
 print("The last five numbers of the array are: {}".format(arr2))
 print("The 2nd through 6th numbers of the array are: {}".format(arr3))
-alexa@ubuntu-xenial:linear_algebra$ ./0-slice_me_up.py 
-The first two numbers of the array are: [9, 8]
-The last five numbers of the array are: [9, 4, 1, 0, 3]
-The 2nd through 6th numbers of the array are: [8, 2, 3, 9, 4]
+
